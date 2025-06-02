fe9ec673790ad7211354f074196e35cdebc37d70
arjsharma <recodetalker@gmail.com>
2025-06-02 22:36:45 +0530

    Fix port setting

--- part3/index.js
+++ part3/index.js
@@ -65,7 +65,7 @@ app.post('/api/persons', (request, response) => {
     });
     
 });
-const PORT = process.env.port;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
