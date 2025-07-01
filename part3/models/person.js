a349891f437ec7b51a14a1ffe7e9b2ab9dbd1300
arjsharma <recodetalker@gmail.com>
2025-07-01 18:10:51 +0530

    Add remaining part 3 exercises

--- part3/models/person.js
+++ part3/models/person.js
@@ -9,8 +9,21 @@ mongoose.connect(MONGO_URI).then(() => {
     console.error('Error connecting to MongoDB:', error.message);
 });
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+    name: {
+    type: String,
+    required: true,
+    minlength: 3},
+    number: {
+    type: String,
+    required: true,
+    minlength: 8,
+    validate: {
+        validator: function(v) {
+            return /\d{2,3}-\d+/.test(v);
+        },
+        message: props => `${props.value} is not a valid phone number!`     
+    }
+}
 });
 personSchema.set('toJSON', {
     transform: (document, returnedObject) => {
